4f7307079e255920004cd5a367d357c94158f0b6
gmamillon <gabrielmamillon@gmail.com>
2021-11-09 11:22:18 +0100

    backend2

--- backend/middleware/validatorEmail.js
+++
@@ -1,9 +0,0 @@
-module.exports = (req, res, next) => {
-    const regexEmail = /^([\w-\.]+)@((?:[\w]+\.)+)([a-zA-Z]{2,4})/i;
-    const regexPassword = /^[a-zA-Z0-9-' éèàêûñçàôëù]{8,64}$/;
-    if (regexEmail.test(req.body.email) && regexPassword.test(req.body.password)) {
-        next();
-    } else {
-        res.status(400).json({ message: "L'adresse mail ou le mot de passe est invalide. (le mot de passe doit faire au moins 8 caractères)"});
-    }
-}
